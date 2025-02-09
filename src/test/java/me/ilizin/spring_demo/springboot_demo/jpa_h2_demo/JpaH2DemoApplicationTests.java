fa2244755a58fab20e21567d32865efdbac359eb
ilizin <ilio.zincone@gmx.com>
2025-02-09 11:25:01 +0100

    project creation

---
+++ src/test/java/me/ilizin/spring_demo/springboot_demo/jpa_h2_demo/JpaH2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ilizin.spring_demo.springboot_demo.jpa_h2_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
